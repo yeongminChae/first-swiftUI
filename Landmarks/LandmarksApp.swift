b2a9cde175010981f56324139204a3ef8c61a4ec
Yeongmin_Chae <codudals98@naver.com>
2023-02-21 18:00:33 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 채영민 on 2023/02/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
